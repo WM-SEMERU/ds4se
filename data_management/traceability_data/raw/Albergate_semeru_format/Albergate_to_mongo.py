a74931bc2769ded0c6625c155028725b38b88764
megretson <megretson@gmail.com>
2020-02-21 13:44:47 -0500

    Updated albergate_to_mongo.py, all documents can now be added

--- data_management/traceability_data/raw/Albergate_semeru_format/Albergate_to_mongo.py
+++ data_management/traceability_data/raw/Albergate_semeru_format/Albergate_to_mongo.py
@@ -17,43 +17,36 @@ def create_documents_from_albergate(ground_truth_file, req_dir, source_dir, db):
                 pairs[line_pair[0]] = list()
                 pairs[line_pair[0]].append(line_pair[1])
 
-    print(pairs)
-
     for requirement in pairs.keys():
 
-        print(requirement)
-        
         req_path = os.path.join(".", req_dir, requirement)
 
         with open(req_path, encoding= "ISO-8859-1") as req_file:
             req_collection = db["requirement_raw"]
             requirement_contents = req_file.read()
 
-        req_document = {"identifier": requirement, "project": "Albergate", "applied_transformations": [],
-                        "ground_truth": [], "file_contents": requirement_contents}
+        req_document = {"name": requirement, "system": "Albergate", "applied_transformations": [],
+                        "ground_truth": [], "contents": requirement_contents}
 
         req_id = req_collection.insert_one(req_document).inserted_id
         
         for source in pairs[requirement]:
-
-            print(source)
 
             source_path = os.path.join(".", source_dir, source)
 
             with open(source_path, encoding="ISO-8859-1") as source_file:
                 source_collection = db["source_raw"]
                 source_contents = source_file.read()
-            source_document = {"identifier": source, "project": "Albergate", "applied_transformations": [],
-                               "ground_truth": [], "file_contents": source_contents}
+            source_document = {"name": source, "system": "Albergate", "applied_transformations": [],
+                               "ground_truth": [], "contents": source_contents}
 
             source_id = source_collection.insert_one(source_document).inserted_id
 
-            # print(req_document, source_document)
             req_query = {"_id": req_id}
-            new_req_values = {"$addToSet": {"ground_truth": source_id}}
+            new_req_values = {"$addToSet": {"ground_truth": ("source_raw", source_id)}}
 
             source_query = {"_id": source_id}
-            new_source_values = {"$addToSet": {"ground_truth": req_id}}
+            new_source_values = {"$addToSet": {"ground_truth": ("requirement_raw", req_id)}}
 
             req_collection.update_one(req_query, new_req_values)
             source_collection.update_one(source_query, new_source_values)
