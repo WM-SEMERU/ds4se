6f221af9bdf529e8df6b4b7d193dc62c34138756
megretson <megretson@gmail.com>
2020-02-23 22:07:57 -0500

    Document validation occurs when inserting new document

--- data_management/Semeru_Collection.py
+++ data_management/Semeru_Collection.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from jsonschema import validate
+from jsonschema import exceptions as json_exceptions
 from json import loads
 
 
@@ -18,22 +19,54 @@ class SemeruCollection(Collection):
         with open(transform_schema) as transform:
             self.transform_schema = loads(transform.read())
 
-
     def insert_one(self, document, bypass_document_validation=False,
                    session=None):
 
+        # validate the document
+        document_type = self.__validate_document(document)
+
+        # if the document is raw, then insert it into the database
+        # NOTE:
+        if document_type == "raw":
+            self.insert_raw_document(document)
+        else:
+            self.insert_transform_document(document)
+
+    def __insert_raw_document(self, document):
+        pass
+
+    def __insert_transform_document(self, document):
+        pass
+
+    def __validate_document(self, document):
         # try to validate against raw schema
         try:
-            validate(document, raw_schema)
-        except Exception as raw_error:
-            pass
-        # if it passes, set type to raw and validate the collection name, so that it is of style XXXX_raw
-        # if it fails, try to validate against transform schema
-        # if it passes, set type to transform and validate the collection name, so that is is of style XXX_transform
-        # if it fails, throw an unrecognized type error, and say it couldn't be validated against either schema. Print
-        # links to the schema
-        #
-        pass
+            validate(document, self.raw_schema)
+
+            # if it passes, set type to raw and validate the collection name, so that it is of style XXXX_raw
+            document_type = "raw"
+            if self.full_name.split("_")[-1] != "raw":
+                raise TypeError("Document validates against raw_schema, but current collection is not a raw collection")
+
+        except json_exceptions.ValidationError as raw_error:
+
+            # if it fails, try to validate against transform schema
+            try:
+                validate(document, self.transform_schema)
+
+                # if it passes, set type to transform and validate the collection name, so that is is of
+                # style XXX_transform
+                document_type = "transform"
+                if self.full_name.split("_")[-1] != "transform":
+                    raise TypeError("Document validates against transform_schema, but current collection is not a "
+                                    "transform collection")
+
+            except json_exceptions.ValidationError as transform_error:
+
+                # if it fails, throw an unrecognized type error, and say it couldn't be validated against either schema.
+                raise TypeError("Document does not validate against raw or transform schema. ",
+                                raw_error, transform_error)
+        return document_type
 
     def delete_one(self, filter, collation=None, session=None):
 
@@ -53,12 +86,6 @@ client = MongoClient('localhost', 27017)
 db = client.test
 test = SemeruCollection(database=db, name="source_raw", raw_schema="./traceability_data/raw_schema.json",
                         transform_schema="./traceability_data/transformed_schema.json")
-print(test.full_name)
-
-
-with open("./traceability_data/raw_schema.json") as f:
-    raw_schema = f.read()
-    raw_schema = loads(raw_schema)
 
 sample = {'name': 'UC58.TXT', 'system': 'eTour', 'applied_transformations': [], 'ground_truth': [{"collection": "Collection_name", "name_and_system": ["eTour", "ground.txt"]}],
           'contents': 'Use case name VISUALIZZASCHEDASITO \nView the details of a particular site. \nPartecipating '
@@ -69,4 +96,6 @@ sample = {'name': 'UC58.TXT', 'system': 'eTour', 'applied_transformations': [], 
                       'conditions \x95 The system displays the details of the selected site. \n\x95 Interruption of '
                       'the connection to the server ETOUR. \nQuality \nrequirements'}
 
-print(validate(sample, raw_schema))
+sample = {"blah": "cool"}
+
+test.insert_one(sample)
