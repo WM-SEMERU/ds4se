648ab78704bd99efdda133ac3d7bc4b92096430c
Ye Yangchen <yye02@email.wm.edu>
2020-10-12 20:07:59 -0400

    formalizing test cases for desc.stat files

--- ds4se/None.py
+++ ds4se/None.py
@@ -43,3 +43,25 @@ from tqdm.notebook import tqdm
 from .mgmnt.prep.bpe import *
 from .exp.info import *
 from .desc.stats import *
+
+# Cell
+# Imports
+import dit
+import math
+import os
+import logging
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import sentencepiece as sp
+
+from collections import Counter
+from pathlib import Path
+from scipy.stats import sem, t
+from statistics import mean, median, stdev
+from tqdm.notebook import tqdm
+
+# ds4se
+from .mgmnt.prep.bpe import *
+from .exp.info import *
+from .desc.stats import *
