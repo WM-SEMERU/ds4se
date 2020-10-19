62881adcb74bedc2f0c6264b4c9dd5c3e57bd451
Alex Harris <caharris02@email.wm.edu>
2020-10-19 00:37:09 -0400

    Re-Refactor Facade.
    Re-refactored facade after problems arose. Rewrote some nominal tests. Errors being thrown will be recorded and discussed with team.
    
    Also during this build I built the library such that we now have access to the working exploratory methods in exp.i. These can be subbed in for the facade methods that are currently in a different form than the rest of the repo.

--- ds4se/None.py
+++ ds4se/None.py
@@ -90,3 +90,35 @@ from .mgmnt.prep.bpe import *
 from .exp.info import *
 >>>>>>> SE_Proj2_Refactor
 from .desc.stats import *
+
+# Cell
+# Imports
+import dit
+import math
+import os
+import logging
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import sentencepiece as sp
+
+from collections import Counter
+from pathlib import Path
+from scipy.stats import sem, t
+from statistics import mean, median, stdev
+from tqdm.notebook import tqdm
+
+# ds4se
+from .mgmnt.prep.bpe import *
+from .exp.info import *
+from .desc.stats import *
+
+# Cell
+import os
+import nbdev
+
+if __name__ == '__main__':
+
+    command_test1 = os.popen("nbdev_test_nbs --fname '.\nbs\1.0_exp.i.ipynb'")
+
+    output = command_test1.read()
