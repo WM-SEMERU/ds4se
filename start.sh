43096bc5babca42df4203a95c99943f8f1112708
Daniel Cardenas <danielrcardenas@gmail.com>
2021-03-30 19:11:33 +0000

    Uploading description evals to DVC

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-PORT=$1
+PORT=8004
 TAG=ds4se
 
 if [ $# -eq 2 ]; then
@@ -17,4 +17,4 @@ fi
 #docker run -d --gpus all -v $(pwd):/tf/main \
 #	-v /mnt/data/ds4se:/tf/data \
 #	-p 8004:8888  $TAG
-docker run --gpus all -d -v /mnt/data/ds4se:/tf/data  -v $(pwd):/tf/main -p $PORT:8888 --name $TAG-$(whoami) $TAG
+docker run --gpus all -d -v /mnt/data/ds4se:/tf/data  -v $(pwd):/tf/main -p $PORT:8888 --name $TAG-danielrc $TAG
